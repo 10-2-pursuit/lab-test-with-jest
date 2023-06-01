51953200a9b67be1a4e9a9f026bac694448dfbf5
Nicole Slater <nicoleslater@pursuit.org>
2023-05-31 22:14:44 -0400

    Working through

---
+++ __tests__/concessions.test.js
@@ -0,0 +1,34 @@
+ const {
+    getConcessionByID,
+    calculateTotalFromIDs
+} = require ("./src/concessions");
+
+const concession = 
+describe('Concessions', () => {
+    test("", () => {
+
+    });
+});
+
+describe('Concessions', () => {
+    test("", () => {
+
+    });
+});
+
+describe('Concessions', () => {
+    test("", () => {
+
+    });
+});
+
+describe('Concessions', () => {
+    test("", () => {
+
+    });
+});
+describe('Concessions', () => {
+    test("", () => {
+
+    });
+});
---
+++ __tests__/tickets.test.js
@@ -0,0 +1,40 @@
+const {
+    getTicketByName,
+    calculateTotalFromTicketNames,
+  } = require("./src/tickets");
+
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
+describe('Tickets', () => {
+    test("", () => {
+
+    });
+});
