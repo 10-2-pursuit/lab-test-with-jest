0783e1ea13722ef214fa6519a354761deba5c9d3
Anile Choice <anilechoice@pursuit.orggit config -- global user.nameresetcd ~git config --global user.name>
2023-06-04 10:23:20 -0400

    completed second test

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -2,12 +2,31 @@ const {
     getTicketByName, 
     calculateTotalFromTicketNames
 } = require("../src/tickets");
+
 const tickets = require("../data/tickets");
 
 describe("getTicketByName()", () => {
-    test("it returns a concession object by ID", () => {
-      const actual = getConcessionByID(concessions, concessions[0].id);
-      const expected = concessions[0];
+    test("returns a ticket object with a matching name", () => {
+      const actual = getTicketByName(tickets, tickets[0].name);
+      const expected = tickets[0];
       expect(actual).toBe(expected);
+    });
+    test("returns null if name does not match", () => {
+        const actual = getTicketByName(tickets, "wrong");
+        const expected = null;
+        expect(actual).toBe(expected);
+    });
+});
+
+describe("calculateTotalFromTicketNames()", () => {
+    test("returns the total value of all tickets from the names array", () => {
+        const actual = calculateTotalFromTicketNames(tickets, ["Adult Matinee", "Senior Matinee"]);
+        const expected = 1788;
+        expect(actual).toBe(expected);
+    });
+    test("returns 0 if names array is incorrect", () => {
+    const actual = calculateTotalFromTicketNames(tickets, []);
+        const expected = 0;
+        expect(actual).toBe(expected);
     });
 });
