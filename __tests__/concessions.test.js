183bb07e6fb37ea3c37b2cdf2984425661606ffc
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-06-04 14:24:11 -0400

    got so help and extra insight on what my confusion

--- __tests__/concessions.test.js
+++ __tests__/concessions.test.js
@@ -1,6 +1,5 @@
-const { getConcessionByID } = require("../src/concessions");
-const { calculateTotalFromIDs } = require("../src/concessions")
-const concessions = require("../data/concessions")
+const { getConcessionByID, calculateTotalFromIDs } = require("../src/concessions");
+const concessions = require("../data/concessions");
 
 
 describe('this will test getConcessionByID', () => {
@@ -15,39 +14,11 @@ describe('this will test getConcessionByID', () => {
     });
 });
 
-// describe('this will test calculateTotalFromIDs', () => {
-//     it('should calculate the total of concessions from given ids', () => {
-//         const id = "g9sZdG1hI";
-//         const actual = calculateTotalFromIDs(concessions, id);
-//         const expected = 929;
-//         expect(actual).toEqual(expected);
-//     });
-    
-//     it('should calculate the total of concessions from given ids', () => {
-//         const id = "0Qs9Yp2NL";
-//         const actual = calculateTotalFromIDs(concessions, id);
-//         const expected = 1029;
-//         expect(actual).toEqual(expected);
-//     });
-
-//     it('should calculate the total of concessions from given ids', () => {
-//         const id = "KzWBehRAD";
-//         const actual = calculateTotalFromIDs(concessions, id);
-//         const expected = 699;
-//         expect(actual).toEqual(expected);
-//     });
-
-//     it('should calculate the total of concessions from given ids', () => {
-//         const id = "NEHAsbTYk";
-//         const actual = calculateTotalFromIDs(concessions, id);
-//         const expected = 749;
-//         expect(actual).toEqual(expected);
-//     });
-
-//     it('should calculate the total of concessions from given ids', () => {
-//         const id = "rNVCeVsri";
-//         const actual = calculateTotalFromIDs(concessions, id);
-//         const expected = 569;
-//         expect(actual).toEqual(expected);
-//     });
-// });
+describe('this will test calculateTotalFromIDs', () => {
+    it('should calculate the total of concessions from given ids', () => {
+        const ids = ["g9sZdG1hI", "KzWBehRAD"];
+        const actual = calculateTotalFromIDs(concessions, ids);
+        const expected = 1628;
+        expect(actual).toEqual(expected);
+    });
+});
--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -1,5 +1,4 @@
-const { getTicketByName } = require("../src/tickets");
-const { calculateTotalFromTicketNames } = require("../src/tickets");
+const { getTicketByName, calculateTotalFromTicketNames } = require("../src/tickets");
 const tickets = require("../data/tickets");
 
 
@@ -18,44 +17,9 @@ describe('this will test getTicketByName', () => {
 
 describe('this will test calculateTotalFromTicketNames', () => {
     it('should calculate the total of tickets from given names', () => {
-        const name = "Adult Matinee";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 949;
-        expect(actual).toEqual(expected);
-    });
-    
-    it('should calculate the total of tickets from given names', () => {
-        const name = "Senior Matinee";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 839;
-        expect(actual).toEqual(expected);
-    });
-
-    it('should calculate the total of tickets from given names', () => {
-        const name = "Child Matinee";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 669;
-        expect(actual).toEqual(expected);
-    });
-
-    it('should calculate the total of tickets from given names', () => {
-        const name = "Adult Regular";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 1369;
-        expect(actual).toEqual(expected);
-    });
-
-    it('should calculate the total of tickets from given names', () => {
-        const name = "Senior Regular";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 1219;
-        expect(actual).toEqual(expected);
-    });
-
-    it('should calculate the total of tickets from given names', () => {
-        const name = "Child Regular";
-        const actual = calculateTotalFromTicketNames(tickets, name);
-        const expected = 1069;
+        const names = ["Adult Matinee", "Senior Matinee"]
+        const actual = calculateTotalFromTicketNames(tickets, names);
+        const expected = 1788;
         expect(actual).toEqual(expected);
     });
 });
